88efe1e335ed9fb77b59e07715acdefe919927e7
dark1709 <darly.leon@alternova.com>
2023-06-27 20:58:26 -0500

    feat:we make the necessary components for the app

--- src/components/CardReport.vue
+++ src/components/CardReport.vue
@@ -1,43 +1,81 @@
-<script setup>
-defineProps({
-  title: String,
-  hours: String,
-  subtitle: String
-})
+<script>
+export default {
+  props: {
+    backgroundTheme: {
+      type: String,
+      required: true
+    },
+
+    backgroundIcon: {
+      type: String,
+      required: true
+    },
+
+    title: {
+      type: String,
+      required: true
+    },
+    hours: {
+      type: String,
+      required: true
+    },
+    subtitle: {
+      type: String,
+      required: true
+    }
+  }
+}
 </script>
 
 <template>
-  <div class="primary-card">
+  <div class="primary-card rounded" :style="{ backgroundColor: backgroundTheme }">
     <picture class="background-card">
-      <img src="../assets/img/icon-work.svg" alt="work" />
+      <img :src="backgroundIcon" alt="work" />
     </picture>
 
-    <article class="card-report">
-      <section class="content">
-        <div class="header-card">
-          <p class="card-title">{{ title }}</p>
-          <span><img src="../assets/img/icon-ellipsis.svg" alt="options" /></span>
-        </div>
-        <h1 class="time">{{ hours }}</h1>
-        <p class="subtitle-previous">{{ subtitle }}</p>
-      </section>
+    <article class="card-report rounded">
+      <div class="card-report__container">
+        <section class="content">
+          <div class="header-card">
+            <p class="card-title">{{ title }}</p>
+            <span><img src="../assets/img/icon-ellipsis.svg" alt="options" /></span>
+          </div>
+          <h1 class="time">{{ hours }}</h1>
+          <p class="subtitle-previous">{{ subtitle }}</p>
+        </section>
+      </div>
     </article>
   </div>
 </template>
 
 <style scoped>
 .primary-card {
-  background-color: hsl(15, 100%, 70%);
-  width: max-content;
-  height: max-content;
-  border-radius: 1.3rem;
-  overflow: hidden;
+  width: 100%;
+  height: 100%;
 }
+
+.background-card {
+  display: flex;
+  justify-content: flex-end;
+  height: 2.5rem;
+  overflow: hidden;
+  position: relative;
+}
+
+.background-card img {
+  position: absolute;
+  right: 1rem;
+  top: -0.5rem;
+}
+
 .card-report {
-  background-color: hsl(235, 46%, 20%);
+  background-color: var(--Dark-blue);
   padding: 1rem;
-  border-radius: 1.3rem;
   color: white;
+}
+
+.card-report__container {
+  margin: 1rem 1.5rem;
 }
 
 .header-card {
@@ -45,73 +83,21 @@ defineProps({
   justify-content: space-between;
   align-items: center;
 }
-/*
-.card-report {
-  height: 100vh;
-  font-family: 'Rubik', sans-serif;
-  background-color: hsl(226, 43%, 10%);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  color: white;
-}
 
-span {
-  justify-self: flex-end;
-}
-
-img {
-  border: 2px solid white;
-  border-radius: 50px;
-  height: 70px;
-  margin-bottom: 30px;
-}
-
-span > img {
-  padding: 3px;
-  border: 0;
-  height: auto;
-}
-
-span > img:hover {
-  cursor: pointer;
-  filter: sepia(100%) grayscale(100%) brightness(100%);
-}
-
-.card {
-  border-radius: 20px;
-  position: relative;
-}
-
-.content {
-  box-sizing: border-box;
-  padding: 25px;
-  background-color: hsl(235, 46%, 20%);
-  width: 100%;
-  height: 80%;
-  border-radius: 20px;
-  position: absolute;
-  bottom: 0;
-  display: grid;
-  grid-template: repeat(3, 1fr) / 1fr 1fr;
-  transition: background-color 0.2s;
-}
-
-.content:hover {
-  background-color: hsl(236, 38%, 33%);
+.card-title {
+  font-size: 1rem;
+  font-weight: 700;
 }
 
 .time {
-  grid-column: span 2;
-  font-size: 48px;
-  font-weight: 300;
+  font-size: 4rem;
+  margin-top: 1.5rem;
+  margin-bottom: 0.5rem;
 }
 
 .subtitle-previous {
-  color: hsl(236, 100%, 87%);
-  font-weight: 300;
-  font-size: 14px;
-  grid-column: span 2;
-  align-self: flex-end;
-} */
+  font-size: 1rem;
+  font-weight: 400;
+  color: var(--Desaturated-blue);
+}
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,19 +7,57 @@ import CardReport from '../components/CardReport.vue'
   <main>
     <div class="cards-container">
       <user-profile name="Jeremy Robson" picture="./src/assets/img/image-jeremy.png" />
+      <CardReport
+        backgroundIcon="./src/assets/img/icon-work.svg"
+        backgroundTheme="var(--Light-red)"
+        title="Work"
+        hours="32hrs"
+        subtitle="Last Week - 36hrs"
+      />
+      <CardReport
+        backgroundIcon="./src/assets/img/icon-play.svg"
+        backgroundTheme="var(--Soft-blue)"
+        title="Play"
+        hours="10hrs"
+        subtitle="Last Week - 8hrs"
+      />
+      <CardReport
+        backgroundIcon="./src/assets/img/icon-study.svg"
+        backgroundTheme="var(--Normal-red)"
+        title="Study"
+        hours="4hrs"
+        subtitle="Last Week - 5hrs"
+      />
+      <CardReport
+        backgroundIcon="./src/assets/img/icon-exercise.svg"
+        backgroundTheme="var(--Lime-green)"
+        title="Exercise"
+        hours="4hrs"
+        subtitle="Last Week - 5hrs"
+      />
+      <CardReport
+        backgroundIcon="./src/assets/img/icon-social.svg"
+        backgroundTheme="var(--Violet)"
+        title="Social"
+        hours="5hrs"
+        subtitle="Last Week - 10hrs"
+      />
+      <CardReport
+        backgroundIcon="./src/assets/img/icon-self-care.svg"
+        backgroundTheme="var(--Soft-orange)"
+        title="Self Care"
+        hours="2hrs"
+        subtitle="Last Week - 2hrs"
+      />
     </div>
   </main>
 </template>
 <style>
-
-
-
-.cards-container{
-        display: grid;
-        grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
-        grid-template-rows: 1fr 1fr;
-        gap: 2rem;
-        width: 80%;
-    }
-
+.cards-container {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  grid-template-rows: 1fr 1fr;
+  gap: 2rem;
+  width: 80%;
+}
 </style>
